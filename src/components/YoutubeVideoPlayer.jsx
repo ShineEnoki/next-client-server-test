4ef546fb4244f374a6428a76c9dd97be2be91981
waiyanthurein <waiyanthurein.lastoenjoy@gmail.com>
2023-08-19 21:13:01 +0630

    first full commit

---
+++ src/components/YoutubeVideoPlayer.jsx
@@ -0,0 +1,36 @@
+"use client"; // This is a client component 👈🏽
+import React, { useEffect, useState } from 'react';
+import ReactPlayer from 'react-player/youtube';
+
+const YoutubeVideoPlayer = ({ videoId, width }) => {
+  const [domLoading, setDomLoading] = useState(false);
+
+  useEffect(() => {
+    setDomLoading(true);
+  }, [])
+
+  return (
+    <>
+      {
+        domLoading && <ReactPlayer
+          url={videoId}
+          width={width}
+          height={width * 6 / 9}
+          controls
+          config={{
+            youtube: {
+              playerVars: {
+                origin: 'http://localhost:3000'
+              }
+            }
+          }}
+          origin={'http://localhost:3000'}
+        />
+      }
+
+    </>
+
+  );
+};
+
+export default YoutubeVideoPlayer;
